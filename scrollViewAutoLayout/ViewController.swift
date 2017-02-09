2999cf254659455cab0a4d9598be94cce1a11361
pradeep-chauhan <prdpchauhan4@gmail.com>
2017-02-09 11:37:25 +0530

    git init

---
+++ scrollViewAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrollViewAutoLayout
+//
+//  Created by Pradeep Chauhan on 12/22/16.
+//  Copyright © 2016 Pradeep Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
